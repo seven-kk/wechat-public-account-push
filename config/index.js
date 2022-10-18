de93d256f32f675bb05e1b357eb1ec6f41bea651
seven-kk <116067271+seven-kk@users.noreply.github.com>
2022-10-18 17:56:59 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ export const config = {
     // 模板消息id
     templateId: "Ksc1IHRHDUvXz1TgWSR8ranIlhiWtLIHUWO-sP68FjI",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: ["o-W2p6LYK1p42tt_-DezLMqpjTmo"],
+    user: ["o-W2p6G3suOXyA0Cv_6egmZOg5Fs"],
      
     // 信息配置
     ewai: "  ",
